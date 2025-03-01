ec2d41064c76e37f05a40b330fc7da545a5b6e9a
pkun2 <pkun2@naver.com>
2025-03-02 05:23:22 +0900

    securityconfig 주석 제거거

--- src/main/java/com/aicodegem/security/SecurityConfig.java
+++ src/main/java/com/aicodegem/security/SecurityConfig.java
@@ -49,16 +49,17 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/**") // 회원가입, 로그인 경로 허용
                         .permitAll()
-                // .requestMatchers("/api/rankings", "/api/code/submissions",
-                // "/api/problems/request",
-                // "/api/problems/request/{id}/approve",
-                // "/api/problems/request/{id}/reject",
-                // "/api/problems", "/api/problems/search",
-                // "/api/users/{userId}/solved-problems")
-                // .permitAll() // 모든 사용자 접근 허용
-                // .requestMatchers("/api/code/submit", "/api/code/resubmit",
-                // "/api/code/revise").authenticated() // 코드
-                // .anyRequest().authenticated() // 나머지 경로는 인증 필요
+                        .requestMatchers("/api/rankings", "/api/code/submissions",
+                                "/api/problems/request",
+                                "/api/problems/request/{id}/approve",
+                                "/api/problems/request/{id}/reject",
+                                "/api/problems", "/api/problems/search",
+                                "/api/users/{userId}/solved-problems")
+                        .permitAll() // 모든 사용자 접근 허용
+                        .requestMatchers("/api/code/submit", "/api/code/resubmit",
+                                "/api/code/revise")
+                        .authenticated() // 코드
+                        .anyRequest().authenticated() // 나머지 경로는 인증 필요
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션리스 방식
