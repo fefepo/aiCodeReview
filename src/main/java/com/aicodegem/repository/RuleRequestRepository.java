3a4fb1ad300224a83e3b2df94b4026a714032365
Hyeon <dlgusdn0129@naver.com>
2025-04-09 23:20:14 +0900

    수정
    수정 1

--- src/main/java/com/aicodegem/repository/RuleRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aicodegem.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.aicodegem.model.RuleRequest;
-
-public interface RuleRequestRepository extends MongoRepository<RuleRequest, String> {
-}
